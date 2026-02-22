aa28596fd32fa1dc3c33bf1ad437d40cc622d7c7
ianivr <ianivrubinstein@gmail.com>
2026-02-22 20:32:18 +0000

    add main

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from textnode import TextNode, TextType
+
+def main():
+    node = TextNode("Hello, World!", TextType.LINK, url="https://example.com")
+    print(node)
+
+if __name__ == "__main__":
+    main()
