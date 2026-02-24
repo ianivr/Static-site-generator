fedc716fd842d3b1fa8a25d02f917ce23977fd27
ianivr <ianivrubinstein@gmail.com>
2026-02-24 20:26:47 +0000

    add ParentNode class with to_html method and corresponding tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -35,3 +35,17 @@ class LeafNode(HTMLNode):
 
     def __repr__(self):
         return f"LeafNode(tag={self.tag}, value={self.value}, props={self.props})"
+
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag: str, children: list, props: dict = {}):
+        super().__init__(tag=tag, value=None, children=children, props=props)
+
+    def to_html(self):
+        if not self.tag:
+            raise ValueError("ParentNode must have a tag")
+        if not self.children:
+            raise ValueError("ParentNode must have children")
+
+        children_html = "".join(child.to_html() for child in self.children)
+        return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
