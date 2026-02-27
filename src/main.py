c9a901a3b6cb816c3101c36d6d84c5da43921650
ianivr <ianivrubinstein@gmail.com>
2026-02-27 20:54:07 +0000

    add generate_page function call in main to process markdown content

--- src/main.py
+++ src/main.py
@@ -1,11 +1,12 @@
 from textnode import TextNode, TextType
-from functions import copy_directory
+from functions import copy_directory, generate_page
 
 def main():
     node = TextNode("Hello, World!", TextType.LINK, url="https://example.com")
     print(node)
 
     copy_directory("static", "public")
+    generate_page("content/index.md", "template.html", "public/index.html")
 
 if __name__ == "__main__":
     main()
